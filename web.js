0d6b3b6eb0ff99008ca0e6e2753f641f3db69713
Anton Ishmurzin <ishmurzin@gmail.com>
2013-07-17 23:28:51 +0000

    added require fs

--- web.js
+++ web.js
@@ -1,6 +1,10 @@
+
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
+
+var fs = require('fs');
 
 var buf = fs.readFileSync("index.html");
 var str = buf.toString('utf-8');
